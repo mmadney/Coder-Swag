310376af1d975a350dd6cb113d59e5b02a43c1ba
mmadney <mohamedmadney1@gmail.com>
2020-02-28 16:11:06 +0200

    create table and cells

---
+++ Coder Swag/Controller/CategoriesVc.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Coder Swag
+//
+//  Created by Mohamed on 2/28/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVc: UIViewController {
+
+    @IBOutlet weak var categTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coder Swag/Views/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  Coder Swag
+//
+//  Created by Mohamed on 2/28/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categLbl: UILabel!
+    @IBOutlet weak var categBackground: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
